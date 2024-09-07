94b0d4042c47d2bcee9e711a25a4c4820c64e2cf
luan048 <luanmonteiropedrosa@gmail.com>
2024-09-06 23:21:39 -0300

    Update

--- potenciaMaquinaC.cpp
+++ potenciaMaquinaC.cpp
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 int main() {
-	int array[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 70, 60, 25, 10, 50, 40, 90, 10, 2};
+	int array[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 70, 60, 2, 25, 10, 50, 40, 90, 10};
 	int tamanho = sizeof(array) / sizeof(array[0]);
 	
-	int maior = 0;
-	int menor = 0;
+	int maior = array[0];
+	int menor = array[0];
 	
 	for(int i = 0; i<tamanho; i++) {
 		if(array[i] > maior) {
 			maior = array[i];
 		}
-		else {
+		if(array[i] < menor) {
 			menor = array[i];
 		}
 	}
